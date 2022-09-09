26c788164a358dc9f020ec3faa53d5742aecf0c9
qiaolian9 <qiaolian9@github.com>
2022-09-09 14:52:38 +0800

    update cudaDeviceProp

--- program_c/ch2/checkDeviceInfo.cu
+++ program_c/ch2/checkDeviceInfo.cu
@@ -2,8 +2,28 @@
 #include<stdio.h>
 
 int main(int argc, char **argv){
+    // device count
     printf("%s starting...\n",argv[0]);
     int deviceCount = 0;
-    cudaError_t error_id = 
+    cudaError_t error_id = cudaGetDeviceCount(&deviceCount);
+
+    if(error_id != cudaSuccess){
+        printf("cudaGetDeviceCount returned %d\n -> %s\n",
+                (int)error_id,cudaGetErrorString(error_id));
+        exit(EXIT_FAILURE);
+    }
+
+    if(deviceCount == 0){
+        printf("There are no available devices that support CUDA\n");
+    }else{
+        printf("Detected %d CUDA Capable devices\n",deviceCount);
+    }
+
+    // device Info (eg device name) 
+    int dev = 3;
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp,dev);
+    printf("Device  %d : %s\n",dev,deviceProp.name);
+
     return 0;
 }
